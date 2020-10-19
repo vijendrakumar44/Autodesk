694780bcded5be02c9f0219c7c3776291026e0c3
ANGEL <ANGEL@192.168.1.9>
2020-10-19 20:32:01 +0530

    it is TE1

--- src/Blank.java
+++ src/Blank.java
@@ -4,6 +4,7 @@ public class Blank {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("it is a blank");
+System.out.println("it is updated one");
 	}
 
 }
